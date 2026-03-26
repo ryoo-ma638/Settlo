6bb4986d35fcadf9a6eea9501907dcd645fd3597
kinoko-1015 <ryohei1774@gmail.com>
2026-03-26 16:51:41 +0900

    homeviewの修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,11 +37,7 @@
     </section>
 
     <section class="ongoing-events">
-<<<<<<< Updated upstream
       <h2 class="section-title" @click="$router.push('/event')">進行中のイベント一覧</h2>
-=======
-      <a @click="$router.push('/event')" class="section-title"><h2>進行中のイベント一覧</h2></a>
->>>>>>> Stashed changes
       <div class="event-list-container">
         <div v-if="ongoingEvents.length === 0" class="empty-message">
           進行中のイベントはありません
