ec80008e04cb304200352b183cfa90e00e50323a
NakahashiRiko <k25096kk@aitech.ac.jp>
2026-03-30 19:51:24 +0900

    相対パスの変更

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', 
+  base: '/Settlo/', 
   plugins: [
     vue(),
     vueDevTools(),
