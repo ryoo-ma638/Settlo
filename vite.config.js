a40aa71018875211c28859e559e2638826f9f6d7
松岡曖來 <ailaisonggang@gmail.com>
2026-03-30 17:50:09 +0900

    Settlo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   // GitHub Pagesで正しくパスを通すための設定
-  base: '/<Settlo>/', 
+  base: '/Settlo/', 
   plugins: [
     vue(),
     vueDevTools(),
